5379cbc2ea72ef26ad262305b46df87c059d67ec
TFY251022 <tiffanylin711@gmail.com>
2025-11-01 16:50:15 +0800

    1101commit

--- Module3/practice.py
+++ Module3/practice.py
@@ -6,10 +6,38 @@
 # 使用者猜對要回傳「恭喜中獎」
 
 
+ans = 57
+
+while True:
+    user_input = input("請輸入1~100的數字:")
+    user_input = int(user_input)
+    if user_input < 1 or user_input > 100:
+        print("超出範圍請重新輸入")
+    elif user_input > ans:
+        print("請輸入更小的數字")
+    elif user_input < ans:
+        print("請輸入更大的數字")
+    else:
+        print("恭喜中獎")
+        break
 
 
 
+ans = 57
+user_input = "aaa"
 
+while user_input != ans :
+    user_input = input("請輸入1~100的數字:")
+    user_input = int(user_input)
+    if user_input < 1 or user_input > 100:
+        print("超出範圍請重新輸入")
+    elif user_input > ans:
+        print("請輸入更小的數字")
+    elif user_input < ans:
+        print("請輸入更大的數字")
+    
+print("恭喜中獎")
+    
 
 
 
